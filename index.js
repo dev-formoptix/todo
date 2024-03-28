db72ad57ad5deadeaa976521ce6d3cdd43531471
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-03-28 18:18:29 +0530

    FO-24 (bug) Database query built from user-controlled sources

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 const mysql = require('mysql');
 const express = require('express');
 const bodyParser = require('body-parser');
-/**
- * @param {string} code The code to evaluate
- * @returns {*} The result of the evaluation
- */
-function evaluateCode(code) {
-    return eval(code); // Alert: Avoid using eval() function
-  }
-  
-  // Example usage triggering the alert
-  evaluateCode("2 + 2");
-  
-const app = express();
 
-// Create connection to MySQL database
+const app = express();
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -22,22 +10,18 @@ const connection = mysql.createConnection({
   database: 'mydatabase'
 });
 
-// Connect to MySQL database
 connection.connect();
 
-// Middleware to parse JSON requests
 app.use(bodyParser.json());
 
-// Endpoint to authenticate user
 app.post('/login', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  // Vulnerable SQL query susceptible to SQL injection
-  const query = `SELECT * FROM users WHERE username = '${username}' AND password = '${password}'`;
+  const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+  const values = [username, password];
 
-  // Execute the SQL query
-  connection.query(query, (err, results) => {
+  connection.query(query, values, (err, results) => {
     if (err) {
       console.error('Error executing query:', err);
       return res.status(500).send('Internal Server Error');
@@ -51,7 +35,6 @@ app.post('/login', (req, res) => {
   });
 });
 
-// Start the server
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
