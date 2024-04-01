75e8fc9c01f18ad87bf307e214075c12db398048
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-04-01 14:12:41 +0530

    FO-428 (bug) Database query built from user-controlled sources

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const express = require('express');
 const bodyParser = require('body-parser');
+const sqlstring = require('sqlstring');
 
 /**
  * @param {string} code The code to evaluate
