2b4669d51559aff409d430cb8c495af1758e7d86
hardik-fabriclake <hardik@fabriclake.com>
2024-07-29 15:13:47 +0530

    testing

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
-  database: 'mydatabase'
+  database: 'mydatabase' 
 });
 
 // Connect to MySQL database
