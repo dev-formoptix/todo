e8ab7537ec08b9251061567fdaadc01cf8276811
hardik-fabriclake <hardik@fabriclake.com>
2024-07-29 17:07:15 +0530

    intit dsds

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ const _ = require('lodash');
 const app = express();
 const port = 3000;
 
-// Safe version of eval using Function constructor
+// Vulnerable endpoint using unsafe eval
 app.get('/eval', (req, res) => {
     const userInput = req.query.input;
-    const code = new Function(userInput); // Safely execute code
-    code(); // Execute the provided code
-    res.send('Eval executed!'); 
+    eval(userInput); // Unsafe use of eval
+    res.send('Eval executed!');
 });
 
-// Properly sanitized endpoint using context-specific escaping
+// Vulnerable endpoint with improper sanitization
 app.get('/user/:username', (req, res) => {
     const username = req.params.username;
-    const message = _.escape(username); // Properly escape user input
+    const message = _.escape(username); // Insecure way to handle user input
     res.send(`Hello ${message}`);
 });
 
