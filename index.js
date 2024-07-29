ba15233ae788e8d13a93c99f3c3271079932920a
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-07-29 17:29:00 +0530

    FO-15 (bug) Database query built from user-controlled sources

--- index.js
+++ index.js
@@ -49,12 +49,10 @@ body
 });
 
 app.get('/search', function handler(req, res) {
-  // BAD: the category might have SQL special characters in it
+  // GOOD: use parameters
   var query1 =
-    "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY='" +
-    req.params.category +
-    "' ORDER BY PRICE";
-  pool.query(query1, [], function(err, results) {
+    "SELECT ITEM,PRICE FROM PRODUCT WHERE ITEM_CATEGORY=$1 ORDER BY PRICE";
+  pool.query(query1, [req.params.category], function(err, results) {
     // process results
   });
 });
