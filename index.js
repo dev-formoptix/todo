13093122dba30ceac55a1e32c746ff689c752d9e
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-08-06 14:42:20 +0530

    FO-167 (bug) Make sure that using this pseudorandom number generator is safe here.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // index.js
-
-let express = require('express');
-let example = express();
+const crypto = require('crypto');
+const express = require('express');
+const example = express();
 
 // Disable x-powered-by header
 example.disable("x-powered-by");
 
 // Securely store credentials outside of the code
-var mysql = require('mysql');
+const mysql = require('mysql');
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.MYSQL_URL,
   user: process.env.MYSQL_USERNAME,
   password: process.env.MYSQL_PASSWORD,
@@ -19,6 +19,9 @@ connection.connect();
 
 // Rest of the code goes here
 ...
+
+// Make sure that using a pseudorandom number generator is safe here
+const randomValue = crypto.randomBytes(1).toString('hex'); // Compliant
 
 // Make sure that executing this OS command is safe here
 const cp = require('child_process');
