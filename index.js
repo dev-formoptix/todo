0178c63a7dcb17602df446d234c267b864b0134c
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-08-07 12:56:53 +0530

    FO-230 (bug) This framework implicitly discloses version information by default. Make sure it is safe here.

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const mysql = require('mysql');
 const { spawn } = require('child_process');
 const crypto = require('crypto');
+const helmet = require('helmet');
 
 const app = express();
 const port = 3000;
+
+// Disable x-powered-by header
+app.disable('x-powered-by');
 
 // MySQL connection setup (replace with your own credentials)
 const connection = mysql.createConnection({
@@ -57,6 +61,9 @@ app.get('/random', (req, res) => {
     const randomNumber = array[0] / Math.pow(2, 32); // Normalize the random number between 0 and 1
     res.send(`Random number: ${randomNumber}`);
 });
+
+app.use(helmet.hidePoweredBy());
+app.use(helmet());
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
