ce89e0f997abbc5415516b4c5f54981d8d9a4bf6
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-08-08 16:53:06 +0530

    FO-298 (bug) Review this potentially hardcoded credential.

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const { spawn } = require('child_process');
 const crypto = require('crypto');
 
 const app = express();
-const port = process.env.PORT || 3000; // Added process.env.PORT to allow dynamic port binding
+const port = process.env.PORT || 3000;
 
-// MySQL connection setup (replace with your own credentials)
+// MySQL connection setup
 const connection = mysql.createConnection({
     host: process.env.MYSQL_URL,
     user: process.env.MYSQL_USERNAME,
@@ -16,30 +16,26 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-// SQL Injection Vulnerable Endpoint
 app.get('/user', (req, res) => {
     const userId = req.query.id;
-    const query = 'SELECT * FROM users WHERE id = ?'; // Use parameterized query to prevent SQL injection
+    const query = 'SELECT * FROM users WHERE id = ?';
     connection.query(query, [userId], (err, results) => {
         if (err) throw err;
         res.send(results);
     });
 });
 
-// Command Injection Vulnerable Endpoint
 app.get('/exec', (req, res) => {
     res.send('Code has been changed to prevent constructing the OS command from user-controlled data.');
 });
 
-// Secure Random Number Generation
 app.get('/random', (req, res) => {
     const array = new Uint32Array(1);
     crypto.randomFillSync(array);
-    const randomNumber = array[0] / 4294967295; // Scaling the random number between 0 and 1
+    const randomNumber = array[0] / 4294967295;
     res.send(`Random number: ${randomNumber}`);
 });
 
-// Disable x-powered-by header
 app.disable('x-powered-by');
 
 app.listen(port, () => {
