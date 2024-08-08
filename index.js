fe8466a40678fd6001d726f9a730cc61cf5f9d2e
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-08-08 13:02:37 +0530

    FO-263 (bug) Make sure that executing this OS command is safe here.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
 const app = express();
 const port = 3000;
@@ -28,12 +29,25 @@ app.get('/user', (req, res) => {
 // Command Injection Vulnerable Endpoint
 app.get('/exec', (req, res) => {
     const cmd = req.query.cmd;
-    exec(cmd, (err, stdout, stderr) => { // Vulnerable to command injection
-        if (err) {
-            res.send(`Error: ${stderr}`);
-            return;
+    const args = cmd.split(" ");
+    const child = spawn(args[0], args.slice(1));
+    let output = "";
+    let error = "";
+  
+    child.stdout.on('data', (data) => {
+        output += data;
+    });
+
+    child.stderr.on('data', (data) => {
+        error += data;
+    });
+
+    child.on('close', (code) => {
+        if (code !== 0) {
+            res.send(`Error: ${error}`);
+        } else {
+            res.send(`Output: ${output}`);
         }
-        res.send(`Output: ${stdout}`);
     });
 });
 
