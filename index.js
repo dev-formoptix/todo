81c0dfc73be875c7f0f5a86439cfd411fe0d76ff
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-08-12 15:17:23 +0530

    FO-4 (bug) Uncontrolled command line

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const { exec } = require('child_process');
+const shellQuote = require('shell-quote');
 
 const app = express();
 const port = 3000;
@@ -18,8 +19,8 @@ connection.connect();
 // SQL Injection Vulnerable Endpoint
 app.get('/user', (req, res) => {
     const userId = req.query.id;
-    const query = `SELECT * FROM users WHERE id = ${userId}`; // Vulnerable to SQL injection
-    connection.query(query, (err, results) => {
+    const query = `SELECT * FROM users WHERE id = ?`; // Use placeholder to prevent SQL injection
+    connection.query(query, [userId], (err, results) => {
         if (err) throw err;
         res.send(results);
     });
@@ -28,7 +29,8 @@ app.get('/user', (req, res) => {
 // Command Injection Vulnerable Endpoint
 app.get('/exec', (req, res) => {
     const cmd = req.query.cmd;
-    exec(cmd, (err, stdout, stderr) => { // Vulnerable to command injection
+    const cmdArgs = shellQuote.parse(cmd); // Parse user input into an array of arguments
+    exec(cmdArgs[0], cmdArgs.slice(1), (err, stdout, stderr) => { // Use an array of arguments to prevent command injection
         if (err) {
             res.send(`Error: ${stderr}`);
             return;
