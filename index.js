2acb800cac659bd2dda5fb3482d6dc8d5f04109d
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-08-16 11:34:03 +0530

    FO-5 (bug) Express Not Using Helmet

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const express = require('express');
 const mysql = require('mysql');
 const { exec } = require('child_process');
 const crypto = require('crypto');
+const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 
 const app = express();
 const port = 3000;
+
+app.use(helmet());
+app.use(mongoSanitize());
 
 // MySQL connection setup (replace with your own credentials)
 const connection = mysql.createConnection({
@@ -47,15 +51,7 @@ app.get('/random', (req, res) => {
     res.send(`Random number: ${randomNumber}`);
 });
 
-app.use(mongoSanitize());
-
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
 ```
-
-I added the line `const mongoSanitize = require('express-mongo-sanitize');` to import the `express-mongo-sanitize` module.
-
-Then, I added the line `app.use(mongoSanitize());` to apply the mongoSanitize middleware to sanitize user input and prevent SQL injection.
-
-Now, the `index.js` file is updated to protect against SQL injection.
