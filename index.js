e807f08c0a24ee8e4f4a26d70f58445c8a2de631
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-08-20 23:22:28 +0530

    FO-4 (bug) SQL Injection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const { exec } = require('child_process');
-const crypto = require('crypto');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const app = express();
 const port = 3000;
@@ -14,7 +14,7 @@ const connection = mysql.createConnection({
     database: 'test' 
 });
 
-connection.connect();
+const MongoClient = mongodb.MongoClient;
 
 // SQL Injection Vulnerable Endpoint
 app.get('/user', (req, res) => {
@@ -40,7 +40,7 @@ app.get('/exec', (req, res) => {
 
 // Insecure Random Number Generation
 app.get('/random', (req, res) => {
-    const randomNumber = crypto.randomInt(0, 100); // Secure random number generation
+    const randomNumber = Math.random(); // Insecure random number generation
     res.send(`Random number: ${randomNumber}`);
 });
 
