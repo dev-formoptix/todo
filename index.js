9a4538d9a62680c0100fdf173af3b79df20b0710
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-08-21 21:15:34 +0530

    FO-5 (bug) Express Not Using Helmet

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 const { exec } = require('child_process');
 const crypto = require('crypto');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 const app = express();
 const port = 3000;
@@ -18,6 +19,7 @@ connection.connect();
 
 app.use(express.json());
 app.use(mongoSanitize());
+app.use(helmet());
 
 app.get('/user', (req, res) => {
     const userId = req.query.id;
