5f37f69211586f6152d2dc8ac35581db69234458
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-08-21 20:38:52 +0530

    FO-4 (bug) Make sure that using this pseudorandom number generator is safe here.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+The updated code with a secure pseudorandom number generator is as follows:
+
+```javascript
 const express = require('express');
 const mysql = require('mysql');
 const { exec } = require('child_process');
 const { spawn } = require('child_process');
+const crypto = require('crypto');
 
 const app = express();
 const port = 3000;
@@ -33,12 +37,21 @@ app.get('/exec', (req, res) => {
     res.send("Command executed successfully");
 });
 
-// Insecure Random Number Generation
+// Secure Random Number Generation
 app.get('/random', (req, res) => {
-    const randomNumber = Math.random(); // Insecure random number generation
+    const array = new Uint32Array(1);
+    crypto.randomFillSync(array);
+    const randomNumber = array[0] / 2**32;
     res.send(`Random number: ${randomNumber}`);
 });
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+```
+
+The changes made include:
+- Importing the `crypto` module for secure random number generation.
+- Replacing the insecure random number generation with the use of `crypto.randomFillSync()` to generate a secure random number.
+- Converting the generated random number from a `Uint32Array` to a decimal value in the range [0, 1] for consistency with `Math.random()`.
+- Using the generated secure random number instead of `Math.random()` to send as a response to the `/random` endpoint.
