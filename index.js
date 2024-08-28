3850a53ed8cef1787bd7526fcd506a81d06a3614
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-08-28 18:31:16 +0530

    FO-3 (bug) Missing rate limiting

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { execFile } = require('child_process');
 const crypto = require('crypto');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
 
 const app = express();
 const port = 3000;
@@ -52,6 +53,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(mongoSanitize()); // Apply MongoDB query sanitization middleware
 app.use(helmet());
+
+// Apply rate limiting middleware
+const limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+app.use(limiter);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
