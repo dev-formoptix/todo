3dc110a5a8c5d8572ce51870492b461a7c99aa6a
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-08-29 11:08:33 +0530

    FO-9 (bug) Command Injection

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+Here's the updated code in the "index.js" file with the command injection vulnerability fixed:
+
+```javascript
 const express = require('express');
 const mysql = require('mysql');
 const { exec } = require('child_process');
@@ -19,8 +22,8 @@ connection.connect();
 // SQL Injection Vulnerable Endpoint
 app.get('/user', (req, res) => {
     const userId = req.query.id;
-    const query = `SELECT * FROM users WHERE id = ${userId}`; // Vulnerable to SQL injection
-    connection.query(query, (err, results) => {
+    const query = `SELECT * FROM users WHERE id = ?`; // Parameterized query to avoid SQL injection
+    connection.query(query, [userId], (err, results) => {
         if (err) throw err;
         res.send(results);
     });
@@ -29,7 +32,9 @@ app.get('/user', (req, res) => {
 // Command Injection Vulnerable Endpoint
 app.get('/exec', (req, res) => {
     const cmd = req.query.cmd;
-    exec(cmd, (err, stdout, stderr) => { // Vulnerable to command injection
+    const cleanCmd = cmd.replace(/[`$();&|]+/g, ''); // Clean the command to avoid command injection
+
+    exec(cleanCmd, (err, stdout, stderr) => {
         if (err) {
             res.send(`Error: ${stderr}`);
             return;
@@ -47,3 +52,12 @@ app.get('/random', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+```
+
+I have made the following changes:
+
+1. In the `/user` endpoint, I converted the SQL query to a parameterized query by replacing the user-supplied `userId` with a `?` placeholder. This helps prevent SQL injection attacks.
+
+2. In the `/exec` endpoint, I added a step to clean the user-supplied `cmd` parameter using a regular expression to remove potentially problematic characters. This helps prevent command injection attacks.
+
+These changes make the code less vulnerable to command injection attacks.
